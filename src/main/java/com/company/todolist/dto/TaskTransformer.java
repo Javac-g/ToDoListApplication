703860d468f4fffcd4473055380abfc531b00d1a
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-09-09 20:48:18 +0300

    Create TaskTransformer.java

---
+++ src/main/java/com/company/todolist/dto/TaskTransformer.java
@@ -0,0 +1,28 @@
+package com.company.todolist.dto;
+
+import com.company.todolist.model.Priority;
+import com.company.todolist.model.State;
+import com.company.todolist.model.Task;
+import com.company.todolist.model.ToDo;
+
+public class TaskTransformer {
+    public static TaskDto convertToDto(Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getName(),
+                task.getPriority().toString(),
+                task.getTodo().getId(),
+                task.getState().getId()
+        );
+    }
+
+    public static Task convertToEntity(TaskDto taskDto, ToDo todo, State state) {
+        Task task = new Task();
+        task.setId(taskDto.getId());
+        task.setName(taskDto.getName());
+        task.setPriority(Priority.valueOf(taskDto.getPriority()));
+        task.setTodo(todo);
+        task.setState(state);
+        return task;
+    }
+}
