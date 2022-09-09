9ff1a50fd811e8696a1f763509ec91c122421015
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-09-09 20:58:54 +0300

    Create UserRepository.java

---
+++ src/main/java/com/company/todolist/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.company.todolist.repository;
+
+import com.company.todolist.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
