52116852d0d15e787ff543eeb07897ed2622d4bf
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-09-10 13:49:57 +0300

    Create RoleServiceImpl.java

---
+++ src/main/java/com/company/todolist/service/impl/RoleServiceImpl.java
@@ -0,0 +1,57 @@
+package com.company.todolist.service.impl;
+
+import com.company.todolist.exception.NullEntityReferenceException;
+import com.company.todolist.model.Role;
+import com.company.todolist.repository.RoleRepository;
+import com.company.todolist.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role create(Role role) {
+        if (role != null) {
+            return roleRepository.save(role);
+        }
+        throw new NullEntityReferenceException("Role cannot be 'null'");
+    }
+
+    @Override
+    public Role readById(long id) {
+        return roleRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Role with id " + id + " not found"));
+    }
+
+    @Override
+    public Role update(Role role) {
+        if (role != null) {
+            readById(role.getId());
+            return roleRepository.save(role);
+        }
+        throw new NullEntityReferenceException("Role cannot be 'null'");
+    }
+
+    @Override
+    public void delete(long id) {
+        Role role = readById(id);
+        roleRepository.delete(role);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return roleRepository.findAll();
+    }
+    public Role getByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
