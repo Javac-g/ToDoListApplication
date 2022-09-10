dd12fb8d9d808768a698d874d045c348c7ac13c7
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-09-10 14:08:10 +0300

    Create NullEntityReferenceException.java

---
+++ src/main/java/com/company/todolist/exception/NullEntityReferenceException.java
@@ -0,0 +1,9 @@
+package com.company.todolist.exception;
+
+public class NullEntityReferenceException extends RuntimeException {
+    public NullEntityReferenceException() {    }
+
+    public NullEntityReferenceException(String message) {
+        super(message);
+    }
+}
