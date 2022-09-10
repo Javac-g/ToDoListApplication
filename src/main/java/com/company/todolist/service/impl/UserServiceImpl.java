5be351aad16dd7f0392e0f76aceb84ee4814b483
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-09-10 13:53:15 +0300

    Create UserServiceImpl.java

---
+++ src/main/java/com/company/todolist/service/impl/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.company.todolist.service.impl;
+
+import com.company.todolist.exception.NullEntityReferenceException;
+import com.company.todolist.model.User;
+import com.company.todolist.repository.UserRepository;
+import com.company.todolist.service.UserService;
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User create(User user) {
+        if (user != null) {
+            user.setPassword(user.getPassword());
+            return userRepository.save(user);
+        }
+        throw new NullEntityReferenceException("User cannot be 'null'");
+    }
+
+    @Override
+    public User readById(long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public User update(User user) {
+        if (user != null) {
+            readById(user.getId());
+            return userRepository.save(user);
+        }
+        throw new NullEntityReferenceException("User cannot be 'null'");
+    }
+
+    @Override
+    public void delete(long id) {
+        User user = readById(id);
+        userRepository.delete(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not Found!");
+        }
+        return user;
+    }
+}
